e1a0c13ad4d126284e5d120c7da198d52676770e
Harshit P G <harshitpg2004@gmail.com>
2024-05-16 12:03:15 +0530

    10

---
+++ 10/hi.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{20, 90, 100, 45, 70}
+
+func sayHi(n string) {
+	fmt.Println("Hi! ", n)
+}
+
+func sayAge(){
+	fmt.Println("Age:", age)
+}
---
+++ 10/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var age= 20
+
+func main() {
+	sayHi("John")
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
+	sayAge()
+}
